bff8c87c6be33d6e754c02f7b564ab7e98a54d97
E1isd <xeliaskeksx@gmail.com>
2026-02-26 12:59:19 +0100

    App.java is back

---
+++ src/main/java/com/katzenrpg/App.java
@@ -0,0 +1,10 @@
+package main.java.com.katzenrpg;
+
+import main.java.com.katzenrpg.GUI.MainWindow;
+
+public class App {
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+        mw.showWindow();
+    }
+}
