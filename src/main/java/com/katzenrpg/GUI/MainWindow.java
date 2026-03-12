2071e8ec16e9b46297fd661b5163e76e330af456
E1isd <xeliaskeksx@gmail.com>
2026-03-12 06:19:37 +0100

    IstJetztFullScreen

--- src/main/java/com/katzenrpg/GUI/MainWindow.java
+++ src/main/java/com/katzenrpg/GUI/MainWindow.java
@@ -1,15 +1,17 @@
 package main.java.com.katzenrpg.GUI;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
 
     public MainWindow() {
         setTitle("KatzenRPG");
-        setSize(1920, 1080); // Auflösung 1920x1080 (Temporär, später anpassbar)
+       /*  setSize(1920, 1080); //Auflösung 1920x1080 (Temporär, später anpassbar) */
+       GraphicsDevice device =
+                GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-
+        device.setFullScreenWindow(this);
         add(new GamePanel());
     }
 
